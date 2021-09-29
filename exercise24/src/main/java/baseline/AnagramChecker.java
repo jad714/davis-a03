51aeea54ba32ba972d55cd58bfbe5ae451a76696
jad714 <josh01100100@gmail.com>
2021-09-29 10:32:41 -0400

    Added Exercise 24 Skeleton

---
+++ exercise24/src/main/java/baseline/AnagramChecker.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+package baseline;
+
+public class AnagramChecker {
+    AnagramChecker(){
+        System.out.println("Welcome to the amazing Anagram Checker!");
+    }
+
+    public char[] inputModifier(String input){
+        // Converts input from Solution24 to lowercase char array with spaces removed.
+    }
+
+    public void sortArray(char[] array) {
+        // Sorts char arrays in preparation for comparison.
+    }
+
+    public boolean isAnagram(String string1, String string2){
+        // Sends strings to inputModifier to convert to char arrays in lowercase without spaces.
+        // Sends char arrays to sortArray to prepare them for comparison.
+        // Compares sorted arrays for equality and returns boolean true/false.
+    }
+
+    public void outputModifier(boolean decider){
+        // If the two strings are anagram of each other, choose corresponding output statement.
+    }
+}
---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,20 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Joshua Davis
+ */
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution24 {
+    // Instantiate new Scanner object (static and final).
+
+   public String scanInput(){
+        // Stores user input to string (return value) for storage in variable.
+    }
+
+    public static void main(String[] args){
+        // Instantiate new AnagramCalculator class.
+        // Call AnagramCalculator's methods in order of appearance to solve problem.
+}
