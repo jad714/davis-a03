ae79606a2a5b1a1a587853687a893e599b8cf2bb
jad714 <josh01100100@gmail.com>
2021-10-02 16:39:07 -0400

    Added Exercise 31 Project Skeleton

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Joshua Davis
+ */
+
+package baseline;
+
+public class Solution31{
+    // Instantiate new object of the Scanner class.
+
+    private void promptScanInput(){
+        // Prompt the user for input.
+    }
+
+    public int validateInput(String input){
+        // Attempt to parse input from user into int. If unable, return to promptInput.
+    }
+
+    public static void main(String[] args){
+        // Call promptScanInput for each required value.
+        // Instantiate new KarvonenHeartRate object, passing all scanned values to its constructor.
+        // Store the result of the calculateTargetHeartRate formula.
+    }
+}
---
+++ exercise31/src/test/java/baseline/Solution31Test.java
@@ -0,0 +1,18 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Joshua Davis
+ */
+
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution31Test {
+
+    @Test
+    void validateInpuTest(){
+        // Ensures that only valid input makes it through (invalid input is trapped and cannot be tested).
+    }
+}
