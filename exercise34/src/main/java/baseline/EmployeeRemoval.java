5011e93e5cf81bc549af3c13fa2d715a7ddc9f94
jad714 <josh01100100@gmail.com>
2021-10-03 15:08:03 -0400

    Completed Exercise 34 (Final)

--- exercise34/src/main/java/baseline/EmployeeRemoval.java
+++ exercise34/src/main/java/baseline/EmployeeRemoval.java
@@ -6,18 +6,42 @@
 package baseline;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeeRemoval{
 
-    public ArrayList<String> createEmployeeList(){
+    public void printList(List<String> employeeList){
+        // Prints the current array list.
+        System.out.println("There are " + employeeList.size() + " employees:");
+        // Loop accesses each element of the employeeList.
+        for(int i=0;i<employeeList.size();i++){
+            System.out.printf("%s%n", employeeList.get(i));
+        }
+        System.out.printf("%n");
+    }
+
+    public List<String> createEmployeeList(){
         // Create the list of employee names here.
+        ArrayList<String> employeeList = new ArrayList<>();
+        employeeList.add("John Smith");
+        employeeList.add("Jackie Jackson");
+        employeeList.add("Chris Jones");
+        employeeList.add("Amanda Cullen");
+        employeeList.add("Jeremy Goodwin");
+        return employeeList;
     }
 
-    public int countEmployees(){
-        // Returns the current count of employees.
-    }
-
-    public ArrayList<String> deleteEmployee(String name){
+    public List<String> deleteEmployee(String name, List<String> employeeList){
         // Deletes an employee from the list matching "name". Returns the updated list.
+        int count = 0;
+        while(count<employeeList.size()){
+            // Making the comparison case-insensitive.
+            if(name.equalsIgnoreCase(employeeList.get(count))){
+                employeeList.remove(count);
+                count-=1;
+            }
+            count++;
+        }
+        return employeeList;
     }
 }
