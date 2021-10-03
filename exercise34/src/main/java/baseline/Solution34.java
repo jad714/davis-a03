ef36fd523f24fe0304512dc5055ac26bf75726a7
jad714 <josh01100100@gmail.com>
2021-10-03 14:51:08 -0400

    Completed Exercise 34

--- exercise34/src/main/java/baseline/Solution34.java
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -1,12 +1,28 @@
 package baseline;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution34{
     // Instantiate new Scanner object.
+    static final Scanner userInput = new Scanner(System.in);
 
     public static void main(String[] args){
         // Instantiate new EmployeeRemoval method.
+        EmployeeRemoval removeEmployee = new EmployeeRemoval();
+        List<String> employeeList;
+        // Creates the list of employees.
+        employeeList = (ArrayList<String>) removeEmployee.createEmployeeList();
         // Print the current list of employees (method call).
+        removeEmployee.printList(employeeList);
         // Scan for user input for the employee to search for and delete.
+        System.out.print("Enter an employee name to remove: ");
+        String name = userInput.nextLine();
+        // Call deleteEmployee to remove the employee from the list.
+        employeeList = removeEmployee.deleteEmployee(name, employeeList);
+        System.out.printf("%n");
         // Print the new list of employees (method call).
+        removeEmployee.printList(employeeList);
     }
 }
--- exercise34/src/test/java/baseline/EmployeeRemovalTest.java
+++ exercise34/src/test/java/baseline/EmployeeRemovalTest.java
@@ -1,23 +1,38 @@
 package baseline;
 
 import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeRemovalTest{
+    // The following object will be used by every test.
+   static final EmployeeRemoval removeEmployeeTest = new EmployeeRemoval();
+    // This field will be used by every test.
+    List<String> employeeListTest = new ArrayList<>();
+        public EmployeeRemovalTest(){
+            employeeListTest.add("John Smith");
+            employeeListTest.add("Jackie Jackson");
+            employeeListTest.add("Chris Jones");
+            employeeListTest.add("Amanda Cullen");
+            employeeListTest.add("Jeremy Goodwin");
+        }
 
     @Test
-    void createEmployeeListTest{
+    void createEmployeeListTest(){
         // This test will ensure the employee list is created correctly.
+        assertEquals(employeeListTest, removeEmployeeTest.createEmployeeList());
     }
 
     @Test
-    void countEmployeeTest{
-        // This test ensures the employee count in a list is correct.
-    }
-
-    @Test
-    void deleteEmployeeTest{
+    void deleteEmployeeTest(){
         // THis test will ensure the employee list is updated correctly following removal of an employee.
+        List<String> removedEmployeeList = new ArrayList<>();
+        removedEmployeeList.add("Jackie Jackson");
+        removedEmployeeList.add("Chris Jones");
+        removedEmployeeList.add("Amanda Cullen");
+        removedEmployeeList.add("Jeremy Goodwin");
+        assertEquals(removedEmployeeList, removeEmployeeTest.deleteEmployee("John Smith", employeeListTest));
     }
 }
