8c289d2ec7949605aeb78082f33c247c56315726
jad714 <josh01100100@gmail.com>
2021-10-03 22:20:48 -0400

    Exercise 38 Project Skeleton Added

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,26 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Joshua Davis
+ */
+
+package baseline;
+
+public class Solution38{
+    // Instantiate new Scanner object.
+
+    public int[] filterEvenNumbers(){
+        // Removes any number in the array that is not even.
+    }
+
+    public int[] parseInput(String[] inputs){
+        // Ensures input is appropriate. Re-scans if not. Returns int array.
+    }
+
+    public static void main(String[] args){
+        // Instantiate new Solution38 object.
+        // Prompt user for appropriate input.
+        // Scan input.
+        // Pass input to parser and store the int array.
+        // Pass int array to filter and print the resulting array.
+    }
+}
---
+++ exercise38/src/test/java/baseline/Solution38Test.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Joshua Davis
+ */
+
+package baseline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution38Test {
+
+    @Test
+    void filterEvenNumbers(){
+        // Test that the related method can return a filtered array.
+    }
+
+    @Test
+    void parseInput(){
+        // Ensure good input makes it through.
+    }
+}
